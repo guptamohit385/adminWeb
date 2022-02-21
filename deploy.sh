2be43fb2fdf6ba13e450871c02af27faf1785332
mgupta <mgupta1@wm.com>
2022-02-21 11:09:03 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,17 @@ sudo docker rm -vf $(sudo docker ps -aqf "name=react_frontend")
 
 echo "-----------------------Step 1 - Container Deleted ------------------------------"
 
-sudo docker rmi -f $(sudo docker images -q ohsc-node-web-app)
+sudo docker rmi -f $(sudo docker images -q node-web-app)
 
 echo "-----------------------Step 2 - Image Deleted ------------------------------"
 
-bash -c "sudo docker build -t ohsc-node-web-app:lastest ."
+bash -c "sudo docker build -t node-web-app:lastest ."
 
 echo "-----------------------Step 3 - Image Build ------------------------------"
 
-sudo docker run -p 8080:3000 -d --name "react_frontend" ohsc-node-web-app:lastest
+sudo docker run -p 8080:3000 -d --name "react_frontend" node-web-app:lastest
 
-echo "-----------------------Step 4 - Deployed ohsc-node-web-app:lastest ------------------------------"
+echo "-----------------------Step 4 - Deployed node-web-app:lastest ------------------------------"
 
 bash -c "sudo docker images"
 
